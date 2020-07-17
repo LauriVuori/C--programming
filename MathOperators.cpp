f5c32f7005ce2b10243b4e20d780d06a571f6978
Lauri Vuori <55624232+Nuubart@users.noreply.github.com>
2020-07-17 10:26:29 +0300

    Use math operators

---
+++ MathOperators.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int num1, num2;
+    
+    cout << "Give first number: ";
+    cin >> num1;
+    cout << "Give second number: ";
+    cin >> num2;
+    cout << num1 <<"+"<<num2<<"="<<num1+num2<< endl;
+
+    cout << num1 <<"-"<<num2<<"="<<num1-num2<< endl;
+    cout << num1 <<"*"<<num2<<"="<<num1*num2<< endl;
+    cout << "Modulo: "<<num1%num2<< endl;
+
+}
