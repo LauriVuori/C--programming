60266d200e133d1b2504600679b73610997c29d5
Lauri Vuori <55624232+LauriVuori@users.noreply.github.com>
2020-07-18 21:15:59 +0300

    Divide numbers

---
+++ CppCourse/NumberDivision.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    double cur1;
+    double cur2 = 5.94573;
+    cout << "Give money:";
+    cin >> cur1;
+    cout << "Sum is: " << cur1/cur2 << endl;
+}
