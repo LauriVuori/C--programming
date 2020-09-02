6bdb47d3a33316fe7c74137489fc97c84c757780
Lauri Vuori <55624232+Nuubart@users.noreply.github.com>
2020-09-02 10:23:31 +0300

    Sort array of struckts

--- CppCourse/StrcktStudentInfo.cpp
+++ CppCourse/StrcktStudentInfo.cpp
@@ -5,26 +5,54 @@
 // in ascending order according to student grade and then print out the content of the array.
 
 #include <iostream>
+#include <ctime>
 using namespace std;
-
+#define ARRLEN 5
 struct StudentInfo {
   char name[25];
   unsigned int id;
   unsigned int grade;
 };
 
-void AddNames(struct Info);
+void sortgrades(struct StudentInfo test[ARRLEN], int ii, int bb);
 
 int main(void){
 
-    struct StudentInfo test[5] =    {
-                                    {{'A','a','p','e','e'}, 2304, 0},
-                                    {{'K','a','l','l','e'}, 1324, 0},
-                                    {{'V','i','l','l','e'}, 5023, 0},
-                                    {{'J','u','l','l','e'}, 1233, 0},
-                                    {{'P','e','r','t','t'}, 6565, 0}           
-                                    };
+    struct StudentInfo test[ARRLEN] =   {
+                                        {{'A','a','p','e','e'}, 1111, 0},
+                                        {{'K','a','l','l','e'}, 2222, 0},
+                                        {{'V','i','l','l','e'}, 3333, 0},
+                                        {{'J','u','l','l','e'}, 4444, 0},
+                                        {{'P','e','r','t','t'}, 5555, 0}           
+                                        };
+    srand(time(NULL));
+    // 1-10
+    // rand() % 10 + 1;
+    for (int ii = 0; ii <= 4; ii++){
+        test[ii].grade = rand() % 10 + 1;
+    }
+
+    int ii = 0;
+    int bb = 0;
     
+    for (ii = 0; ii < ARRLEN; ii++){
+        for (bb = ii+1; bb < ARRLEN; bb++){
+            if (test[ii].grade > test[bb].grade){
+                sortgrades(test, ii, bb);
+            }
+
+        }
+    }
+    for (int ii = 0; ii < ARRLEN; ii++){
+        cout << "Student: " << test[ii].name << " id: " << test[ii].id << " grade: " << test[ii].grade << endl;
+    }
+}
+
+void sortgrades(struct StudentInfo test[ARRLEN], int ii, int bb){
+    struct StudentInfo temp;
+    temp = test[ii];
+    test[ii] = test[bb];
+    test[bb] = temp;
 
 }
 
