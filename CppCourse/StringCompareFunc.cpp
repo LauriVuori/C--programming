a64a69303a151700853521042dbe8c4acb078ccc
LauriVuori <Laurivuori.lv@gmail.com>
2020-09-10 09:44:23 +0300

    Compare strings, getline, strcmp

--- CppCourse/StringCompareFunc.cpp
+++ CppCourse/StringCompareFunc.cpp
@@ -5,22 +5,14 @@ using namespace std;
 
 
 int main(void){
-    char names[5][25], temp[25];
+    const int ARRLEN = 5, TEXTLEN = 25;
+    char names[ARRLEN][TEXTLEN], temp[25];
     int ii = 0;
     int bb = 0;
 
-    for (ii = 0; ii < 5; ii++){
+    for (ii = 0; ii < ARRLEN; ii++){
         cout << "Give name " << ii+1 << ":";
-        cin >> names[ii];
-        if ((names[ii][0] >= 'a') && (names[ii][0] <= 'z')){
-            names[ii][0] -= 32;
-        }
-        for (int jj = 1; names[ii][jj] != '\0'; jj++) {
-            if ((names[ii][jj] >= 'A') && (names[ii][jj] <= 'Z')) {
-                names[ii][jj] += 32;
-            }
-
-        }
+        cin.getline(names[ii], TEXTLEN, '\n');
     }
 
 
@@ -29,17 +21,10 @@ int main(void){
 
     for (ii = 0; ii < 5; ii++){
         for (bb = ii+1; bb < 5; bb++){
-            if (strncmp(names[ii], names[bb], 1) > 0){
+            if (strcmp(names[ii], names[bb]) > 0){
                 strcpy (temp, names[ii]);
                 strcpy (names[ii], names[bb]);
                 strcpy (names[bb], temp);
-            }
-            else if (strncmp(names[ii], names[bb], 1) == 0){
-                if (strncmp(names[ii], names[bb], 2) > 0){
-                    strcpy (temp, names[ii]);
-                    strcpy (names[ii], names[bb]);
-                    strcpy (names[bb], temp);
-                }
             }
         }
     }
