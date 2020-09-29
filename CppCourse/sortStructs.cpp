edb0b397f8d5d22f568772b0c344c99460fe8477
Lauri Vuori <55624232+LauriVuori@users.noreply.github.com>
2020-09-29 20:59:43 +0300

    Sort structs

---
+++ CppCourse/sortStructs.cpp
@@ -0,0 +1,82 @@
+/*A program in which you define a struct for customer information. The fields in the struct should be name, 
+id and the amount of purchase. The program should read the information of a group of customers to an array 
+and then display the information of the customers 
+in descending order according to the purchase amounts. Use a function for reading the customer information 
+and one for sorting the customers.*/
+
+/*
+1. group of customers to an array,
+2. display info of customers
+3. descending order purchase amounts
+*/
+
+#include <iostream>
+
+#define ARSIZE 50
+
+using namespace std;
+
+struct customers{
+    char name[ARSIZE];
+    char customerId[ARSIZE];
+    double totalCost;
+};
+
+int allocateMem(customers *); 
+void readCustomerData(customers *, int);
+void printCustomerData(customers *, int);
+void sortCustomers(customers *, int);
+
+int main(void){
+    int numberOfCustomers = 0;
+    customers * customerData;
+
+    numberOfCustomers = allocateMem(customerData);
+    readCustomerData(customerData, numberOfCustomers);
+    printCustomerData(customerData, numberOfCustomers);
+
+    // delete [] customerData;
+    // cout << customerData[1].totalCost << endl;
+}
+
+int allocateMem(customers * customerData){
+    int numberOfCustomers = 0;
+    cout << "How many customers there is: " << endl;
+    cin >> numberOfCustomers;
+    cin.get();
+    try {
+    customerData = new customers[numberOfCustomers];
+    }
+    catch (bad_alloc xa) {
+       cout<<"Memory allocation Failed!";
+    }
+    return numberOfCustomers;
+}
+
+void readCustomerData(customers * customerData, int numberOfCustomers){
+    cout << "\nGive customer information" << endl;
+    for (int i = 0; i < numberOfCustomers; i++){
+        cout << "Customer " << i+1 << ":" << endl;
+        cout << "Give customer name: " << endl;
+        cin.getline(customerData[i].name, ARSIZE, '\n');
+        cout << "Give customer id: " << endl;
+        cin.getline(customerData[i].customerId, ARSIZE, '\n');
+        cout << "Give customers purchase total cost: " << endl;
+        cin >> customerData[i].totalCost;
+        cin.get();
+    }
+}
+
+void printCustomerData(customers * customerData, int numberOfCustomers){
+    cout << "Customers:" << endl;
+    for (int i = 0; i < numberOfCustomers; i++){
+        cout << "Customer " << i+1 << ":" << endl;
+        cout << "Name: " << customerData[i].name << endl;
+        cout << "Id: " << customerData[i].customerId << endl;
+        cout << "Total cost: " << customerData[i].totalCost << "e" << endl;
+    }
+}
+
+void sortCustomers(customers * customerData, int numberOfCustomers){
+
+}
