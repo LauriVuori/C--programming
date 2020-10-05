bd57c5e3449b29acf2082dfefab8ff785f067e48
Lauri Vuori <55624232+LauriVuori@users.noreply.github.com>
2020-10-05 10:48:45 +0300

    print arguments from command line

--- CppCourse/printArgumentsFromCmd.cpp
+++ CppCourse/printArgumentsFromCmd.cpp
@@ -3,11 +3,10 @@ using namespace std;
   
 int main(int argc, char** argv) { 
 
-    cout << "Name of program is: " << argv[0] << endl;
-    for (int i = 0; i < argc; i++){
-        cout << "argv " << i << ":" << argv[i] << "\n";
-    }
-    cout << endl;
+    // for (int i = 0; i < argc; i++){
+    //     cout << "argv " << i << ":" << argv[i] << "\n";
+    // }
+    // cout << endl;
 
     cout << "You have entered " << argc 
          << " arguments:" << "\n"; 
