041ebd79f858c2791b21a93493e82567a8050122
Lauri Vuori <55624232+LauriVuori@users.noreply.github.com>
2020-10-06 13:48:35 +0300

    Use recursion in counting

--- CppCourse/recursion.cpp
+++ CppCourse/recursion.cpp
@@ -5,21 +5,57 @@
 #include <iostream>
 using namespace std;
 
-int main(void){
-    int counter = 0, i = 0;
-    double sum = 0;
+double firstRecursion(double);
+double secondRecursion(double);
 
-    cout << "How many times:" << endl;
-    cin >> counter;
-    cin.get();
+int main(void) {
+	double number, result1, result2;
 
-    for (i = 1; i <= counter; i++){
-        sum += 1.0/(2.0*i);
-    }
-    cout << "Sum is: " << sum << endl;
-    sum = 1;
+	cout << "Type an integer number: ";
+	cin >> number;
+	if (number < 0)
+		return -1;
+
+	result1 = firstRecursion(number);
+	
+    result2 = secondRecursion(number);
+    cout << endl << "first result is: " << result1 << endl;
+    cout << "second result is: " << result2 << endl;
+
+	return 0;
+}
+
+
+double firstRecursion(double number) {
+	double value = 0;
+	if (number == 1.0) {
+		cout << "number: " << number << "value: " << (1.0/(2.0 * number)) << endl;
+        return (1.0 / (2.0 * number));
+	}
+
+    value = firstRecursion(number - 1.0) + 1.0/(2.0 * number);
+	cout << "Num: " << number << " value: " << value <<endl;
+	return(value);
+}
+
+double secondRecursion(double number) {
+	double value = 0;
+	if (number == 1.0) {
+		cout << "number: " << number << "value: " << (1.0/(2.0 * number)) << endl;
+        return (-number/(2.0 * number + 1.0));
+	}
+
+    value = secondRecursion(number - 1.0) * -number/(2.0 * number + 1.0);
+	cout << "Num: " << number << " value: " << value <<endl;
+	return(value);
+}
+
+/*
     for (i = 1.0; i <= counter; i++){
         sum *= -i/(2.0*i+1.0);
     }
-    cout << "Multiplication is: " << sum << endl;
-}
+*/
+
+/*
+Sum is: 1.14167
+Multiplication is: -0.011544*/
