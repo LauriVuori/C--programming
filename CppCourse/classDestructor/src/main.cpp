23143dd2e7b51ab52624ce2c2937e12d2e06228d
Lauri Vuori <55624232+LauriVuori@users.noreply.github.com>
2020-11-08 21:59:02 +0200

    destructors

---
+++ CppCourse/classDestructor/src/main.cpp
@@ -0,0 +1,35 @@
+/*
+A program for managing the information of IT users. For this purpose, 
+1define a class called User, which should have the following private attributes: 
+first name, surname, phone number, username and password. 
+Define necessaty constructors and destructor for the class. The destructor should display the username and password when it is called. Define also the following methods for the class:
+-get_info() method, which returns as text all user information except for the password.
+-search() method, which receives the username as an argument and returns all the user information if the given username is the same as the user's username. 
+
+Define also class Admin as a friend class to class User. The attributes of class Admin should be username and password. Define necessary constructors and the destructor for this class too. The destructor should display a message saying that Admin object is being destroyed. Define also the following methods for the class:
+set_usrname() method, which receives as argument a User object and a username and sets the username of the given object.
+set_password() method, which receives as argument a User object and a password and sets the password of the given object.
+In the main() function of the program create an array of User objects and an Admin object and pass User objects to the Admin object to initialize them. 
+After this call get_info() and search() methods of User objects to test their functionality. 
+Hint: To convert text to integere and decimal values you can use atoi(const char *_string) and atof(const char *_string) functions. 
+Respectively, numbers can be converted to text using int sprintf (char * str, const char * format, double number) function, 
+which returns the number of characters of the text after conversion.
+
+Class user, attributes:
+first name, surname, phone number, username password,
+constructors, destructors.
+!destructor should display the username and password when it is called.
+-get_info() method, which returns as text all user information except for the password.
+-search() method, which receives the username as an argument and returns all the user information if the given username is the same as the user's username.
+
+class admin, friend class to user
+attributes:
+username and password
+set_usrname() method, which receives as argument a User object and a username and sets the username of the given object.
+set_password() method, which receives as argument a User object and a password and sets the password of the given object.
+
+
+In the main() function of the program create an array of User objects and an Admin object and pass User objects to the Admin object to initialize them. 
+After this call get_info() and search() methods of User objects to test their functionality. 
+
+*/
